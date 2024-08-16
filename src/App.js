015f9949ce4975101249f7b1a19819997e017bca
sparkjames <jpfleiderer@gmail.com>
2024-08-16 15:07:42 -0400

    find nimbus font

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.scss';
+import './assets/fonts/nimbus-roman-no9-l.css';
 
 import Header from './components/header/header.component';
 import Hero from './components/hero/hero.component';
 import RecentPosts from './components/recentPosts/recentPosts.component';
 import Footer from './components/footer/footer.comonent';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect( () => {
+    const WebFont = require('webfontloader');
+
+    WebFont.load({
+      google: {
+        families: [
+          'Oswald:500'
+        ]
+      },
+      custom: {
+        families: ['Nimbus Roman No9 L Regular', 'Nimbus Roman No9 L Bold']
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <Header></Header>
