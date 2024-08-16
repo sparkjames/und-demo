240e2f726688e8b1668cc3411209be465572f8ab
sparkjames <jpfleiderer@gmail.com>
2024-08-16 14:51:36 -0400

    setting up hero styles

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -5,29 +5,36 @@ import HeroContent from '../../content/hero.json';
 const Hero = () => {
 	return (
 		<section className="hero">
-      <div className="hero-container container">
 
-        <h1 className="hero-primaryHeading">
-					{HeroContent.primaryHeading}
-				</h1>
+			<h1 className="hero-primaryHeading">
+				{HeroContent.primaryHeading}
+			</h1>
 
-				{ HeroContent.content.map( (heroSection, i) => {
-					return (
-						<div key={i} className={`hero-message hero-message-${i}`}>
-							<h2 className="hero-heading">{heroSection.heading}</h2>
-							{ heroSection.subheading && 
-							<p className="hero-subheading">{heroSection.subheading}</p>
+			{ HeroContent.content.map( (heroSection, i) => {
+				const { heading, subheading, description, backgroundImage, backgroundColor } = heroSection;
+				return (
+					<div key={i} className={`hero-message hero-message-${i}`} style={{backgroundColor: backgroundColor}}>
+						
+						{ backgroundImage &&
+						<img className="hero-backgroundImage" src={backgroundImage} width="1280" height="720" alt="TODO temporary placeholder" />
+						}
+
+						<div className="hero-messageContainer container">
+							<h2 className="hero-heading">{heading}</h2>
+							{ subheading && 
+							<p className="hero-subheading">{subheading}</p>
 							}
-							<p className="hero-description">{heroSection.description}</p>
+							<p className="hero-description">{description}</p>
 						</div>
-					);
-				}) }
 
-        <div className="hero-cta">
-          <a href={HeroContent.cta.url}className="hero-ctaLink">{HeroContent.cta.title}</a>
-        </div>
+					</div>
+				);
+			}) }
 
-      </div>
+			<div className="hero-cta">
+				<a href={HeroContent.cta.url}className="hero-ctaLink">{HeroContent.cta.title}</a>
+			</div>
+
     </section>
 	);
 };
