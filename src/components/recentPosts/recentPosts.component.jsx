a8637652e9a20d50fc20bd039812bf5a010e9a18
sparkjames <jpfleiderer@gmail.com>
2024-08-16 14:22:38 -0400

    get date for endpoint dynamically

--- src/components/recentPosts/recentPosts.component.jsx
+++ src/components/recentPosts/recentPosts.component.jsx
@@ -9,9 +9,22 @@ const RecentPosts = () => {
 	const [posts, setPosts] = useState([]);
 
 	useEffect( () => {
+
+		/**
+		 * The social media endpoint needs a start date and end date 
+		 * in YYYY-MM-DD format. Use today's date and the date from
+		 * two days ago for these values.
+		 */
+		const todayDate = new Date();
+		const twoDaysAgoDate = new Date();
+		twoDaysAgoDate.setDate( twoDaysAgoDate.getDate() - 2);
+
+		const startDate = twoDaysAgoDate.toISOString().split('T')[0];
+		const endDate = todayDate.toISOString().split('T')[0];
+
 		const fetchPosts = async () => {
 			
-			fetch("https://apps.und.edu/demo/public/index.php/post?from=2024-08-12&to=2024-08-14")
+			fetch(`https://apps.und.edu/demo/public/index.php/post?from=${startDate}&to=${endDate}`)
 				.then( (response) => response.json() )
 				.then( (newPosts) => setPosts( Array.from(newPosts) ) )
 				.catch( (error) => console.error(error) );
