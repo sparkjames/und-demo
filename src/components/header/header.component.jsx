1d98c6b31358d11e4f0b1d4a156eee446b387ac6
sparkjames <jpfleiderer@gmail.com>
2024-08-17 20:35:25 -0400

    fixed top menu

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,28 @@ import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/images/logotype-full-compressed.svg';
 
 import HeaderNav from '../../content/header-nav.json';
+import { useEffect } from 'react';
 
 const Header = () => {
+
+	useEffect( () => {
+		// There is a CSS custom property `--header-height` that controls the size/position of the mega nav. The height must be updated when the window is resized or scrolled, since the height of the header can change after either of those events.
+		const update_header_height_property = () => {
+			let header_height = document.querySelector('.siteHeader').clientHeight;
+			if( header_height ){
+				document.documentElement.style.setProperty( '--header-height', header_height + 'px' );
+			}
+		};
+
+		document.addEventListener('DOMContentLoaded', function() {
+			update_header_height_property();
+		}, false);
+
+		window.addEventListener('resize', update_header_height_property, {passive: true});
+		window.addEventListener('scroll', update_header_height_property, {passive: true});
+
+	}, []);
+
 	return (
 		<header className="siteHeader">
 			<div className="siteHeader-container container">
