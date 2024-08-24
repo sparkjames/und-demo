9437f36e2c4986790182dc14b5ade0e52ed4a737
sparkjames <jpfleiderer@gmail.com>
2024-08-24 17:04:47 -0400

    update hero images, add alts, change backgroundImage to contentImage

--- src/components/heroMessage/heroMessage.component.jsx
+++ src/components/heroMessage/heroMessage.component.jsx
@@ -1,9 +1,7 @@
 import './heroMessage.styles.scss';
 
 const HeroMessage = ( {message} ) => {
-	const { heading, subheading, description, contentImage, backgroundImage, backgroundColor, textColor } = message;
-	
-	// TODO: The image around here called "backgroundImage" turned into a "slideImage", change the name when possible.
+	const { heading, subheading, description, contentImage, imageAlt, backgroundColor, textColor } = message;
 
 	return (
 		<div className={`hero-message hero-message--${textColor}`} style={{backgroundColor: backgroundColor}}>
@@ -11,17 +9,13 @@ const HeroMessage = ( {message} ) => {
 
 				<div className="hero-messageArrow"></div>
 
-				{ backgroundImage &&
-				<div className="hero-backgroundImageContainer">
-					<img className="hero-backgroundImage" src={ backgroundImage } width="1280" height="720" alt="TODO temporary placeholder" />
+				{ contentImage &&
+				<div className="hero-contentImageContainer">
+					<img className="hero-contentImage" src={ contentImage } width="1280" height="720" alt={imageAlt} />
 				</div>
 				}
 
 				<div className="hero-contentContainer">
-
-					{ contentImage &&
-					<img className="hero-contentImage" src={ contentImage } width="268" height="400" alt="TODO temporary placeholder" />
-					}
 
 					<h2 className="hero-heading">{heading}</h2>
 					{ subheading && 
