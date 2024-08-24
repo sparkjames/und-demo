755459c66635733c24248590c8b1d6abc67e68ad
sparkjames <jpfleiderer@gmail.com>
2024-08-24 14:06:40 -0400

    add toggle for condensed/detailed

--- src/components/recentPosts/recentPosts.component.jsx
+++ src/components/recentPosts/recentPosts.component.jsx
@@ -1,5 +1,8 @@
 import './recentPosts.styles.scss';
 
+import GridOnIcon from '@mui/icons-material/GridOn';
+import GridViewIcon from '@mui/icons-material/GridView';
+
 import { useEffect, useState, useContext } from 'react';
 
 import { TagFiltersContext } from '../../contexts/tagFilters.context';
@@ -19,6 +22,7 @@ const RecentPosts = () => {
 
 	const [posts, setPosts] = useState([]);
 	const [filteredPosts, setFilteredPosts] = useState([]);
+	const [postViewType, setPostViewType] = useState('detailed');
 
 	// Get the array of selected tags to filter the grid of posts.
 	const { selectedTags } = useContext( TagFiltersContext );
@@ -86,11 +90,33 @@ const RecentPosts = () => {
 
 	}, [posts, postSearchFilter, selectedTags]);
 
+	const viewControlOnChange = (e) => {
+		setPostViewType( e.target.value );
+	};
+
 	return (
-		<section className="recentPosts">
+		<section className={`recentPosts recentPosts--${postViewType}`}>
 			<div className="recentPosts-container container">
 
-				<h2 className="recentPosts-primaryHeading">Recent Posts</h2>
+				<header className="recentPosts-header">
+
+					<h2 className="recentPosts-primaryHeading">Recent Posts</h2>
+
+					<div className="recentPosts-viewControls">
+
+						<label className="recentPosts-viewControl recentPosts-viewControl--condensed" title="Switch to Condensed View">
+							<input type="radio" className="recentPosts-viewControlRadio" name="viewControl" value="condensed" onClick={viewControlOnChange} />
+							<i className="recentPosts-viewControlIcon"><GridOnIcon></GridOnIcon></i>
+						</label>
+
+						<label className="recentPosts-viewControl recentPosts-viewControl--detailed" title="Switch to Detailed View">
+							<input type="radio" className="recentPosts-viewControlRadio" name="viewControl" value="detailed" onClick={viewControlOnChange} defaultChecked />
+							<i className="recentPosts-viewControlIcon"><GridViewIcon></GridViewIcon></i>
+						</label>
+
+					</div>
+
+				</header>
 
 				<div className="recentPosts-body">
 
